9f7923ca5849bb10a4e26024b1befc9628792206
aji sasongko <57887588+jisong710@users.noreply.github.com>
2022-07-04 11:24:20 +0700

    Update algoritma_genetika.py

--- module/algoritma_genetika.py
+++ module/algoritma_genetika.py
@@ -553,7 +553,7 @@ class algoritma_genetik:
         print("Total Nilai fitness terbaik generasi ke-",m,":", fitnessterbaik[l])
         print("Total Biaya terbaik generasi ke-",m,":", Biaya[l])
         print("Kromosom terbaik generasi ke-",m,":", kromosomterbaik[l])
-        for i in range len(nama_device):
+        for i in range(len(nama_device)):
           data = { "m2m:cnt": {"rn": nama_device[i]}}
           headers = {"X-M2M-Origin":"5ea7fcd6a0249308:bcd27b2f5f71065f","Content-Type":"application/json;ty=3","Accept":"application/json"}
           requests.post('https://platform.antares.id:8443/~/antares-cse/antares-id/PenjadwalanListrik', headers=headers , data=data)
